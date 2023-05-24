fa8e80cbb2561fa5e26e5137827e5124c8e1bd0d
Eduard Tanase <eduard.tanase1@gmail.com>
2023-05-24 14:48:15 -0700

    feat: Use react-router-dom on new event

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import React from 'react';
 import { Button, Col, Container, Row } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Dashboard = () => {
   return (
@@ -11,9 +12,11 @@ const Dashboard = () => {
             <h1>Dashboard</h1>
           </Col>
           <Col xs={2}>
-            <Button className="md-6" variant="primary" href="/events/new">
-              New Event
-            </Button>
+            <LinkContainer to="/events/new">
+              <Button className="md-6" variant="primary">
+                New Event
+              </Button>
+            </LinkContainer>
           </Col>
         </Row>
         <Outlet />
